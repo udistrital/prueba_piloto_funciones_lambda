3c3d799d5a767a2a253c0d976a7d34a9c59a7e96
edwargl7 <07edwargl@gmail.com>
2023-07-30 22:32:15 -0500

    feat: udistrital/sga_cliente#1370 agregada función put

--- src/handlers/put_syllabus/app.py
+++ src/handlers/put_syllabus/app.py
@@ -1,9 +1,13 @@
-# Get one syllabus
+# Put syllabus
 
 import json
 import os
+from datetime import datetime
+from typing import List, Dict, Optional
 
+import pytz
 from bson import ObjectId
+from pydantic import BaseModel, Field
 from pymongo import MongoClient
 
 SYLLABUS_CRUD_HOST = os.environ.get('SYLLABUS_CRUD_HOST')
@@ -11,6 +15,30 @@ SYLLABUS_CRUD_PORT = os.environ.get('SYLLABUS_CRUD_PORT')
 SYLLABUS_CRUD_USERNAME = os.environ.get('SYLLABUS_CRUD_USERNAME')
 SYLLABUS_CRUD_PASS = os.environ.get('SYLLABUS_CRUD_PASS')
 SYLLABUS_CRUD_DB = os.environ.get('SYLLABUS_CRUD_DB')
+TIMEZONE = os.environ.get('TIMEZONE')
+COLLECTION = "syllabus"
+
+print(SYLLABUS_CRUD_DB)
+
+
+def local_now():
+    return datetime.now(tz=pytz.timezone(TIMEZONE))
+
+
+class SyllabusModel(BaseModel):
+    espacio_academico_id: int
+    justificacion: str
+    objetivo_general: str
+    objetivos_especificos: List
+    resultados_aprendizaje: List
+    articulacion_resultados_aprendizaje: str
+    contenido: Dict
+    estrategias: List
+    evaluacion: Dict
+    bibliografia: List
+    seguimiento: List
+    activo: bool = Field(default=True)
+    fecha_modificacion: Optional[datetime] = Field(default=local_now())
 
 
 def connect_db_client():
@@ -42,6 +70,13 @@ def close_connect_db(client):
         print(f"Detail: {ex}")
 
 
+def parse_body(event) -> tuple:
+    try:
+        return json.loads(event["body"]), None
+    except Exception as ex:
+        return None, ex
+
+
 def format_response(result, message: str, status_code: int, success: bool):
     if isinstance(result, dict):
         if success:
@@ -49,6 +84,8 @@ def format_response(result, message: str, status_code: int, success: bool):
                 result["_id"] = str(result["_id"])
             if result.get("fecha_creacion"):
                 result["fecha_creacion"] = str(result["fecha_creacion"])
+            if result.get("fecha_modificacion"):
+                result["fecha_modificacion"] = str(result["fecha_modificacion"])
 
             return {"statusCode": status_code,
                     "body": json.dumps({
@@ -71,38 +108,42 @@ def lambda_handler(event, context):
     try:
         syllabus_id = event["pathParameters"]["id"]
         print(syllabus_id)
-        client = connect_db_client()
-        if client:
-            print("Connecting database ...")
-            syllabus_collection = client[str(SYLLABUS_CRUD_DB)]["syllabus"]
-            print("Connection database successful")
-            syllabus = syllabus_collection.find_one({
-                "_id": ObjectId(syllabus_id)
-            })
-            print(f"Consulted record.")
-            if syllabus:
-                print("Syllabus found!")
-                print(syllabus)
-                print(type(syllabus))
-                return format_response(
-                    syllabus,
-                    "Syllabus OK",
-                    200,
-                    True)
-            else:
-                print("Syllabus not found!")
-                close_connect_db(client)
-        return format_response(
-            {},
-            "Error get syllabus!",
-            403,
-            False)
+        data, error = parse_body(event)
+        if error is None:
+            # Validate structure
+            syllabus_data = SyllabusModel(**data).__dict__
+            print(syllabus_data)
+            client = connect_db_client()
+            filter_ = {"_id": ObjectId(syllabus_id)}
+            if client:
+                print("Connecting database ...")
+                syllabus_collection = client[str(SYLLABUS_CRUD_DB)]["syllabus"]
+                print("Connection database successful")
+                print("Updating syllabus")
+                result = syllabus_collection.update_one(
+                    filter_,
+                    {"$set": syllabus_data})
+                print(f"Updated syllabus {syllabus_id}")
+                if result:
+                    syllabus = syllabus_collection.find_one(filter_)
+                    return format_response(
+                        syllabus,
+                        "Updated syllabus",
+                        200,
+                        True)
+                else:
+                    close_connect_db(client)
+            return format_response(
+                {},
+                "Error updating syllabus!",
+                403,
+                False)
     except Exception as ex:
-        print("Error get syllabus")
+        print("Error updating syllabus")
         print(f"Detail: {ex}")
         close_connect_db(client)
         return format_response(
             {},
-            "Error get syllabus!",
+            "Error updating syllabus!",
             403,
             False)
